87880a6d96b2e7adf10af4022fd4acefa4802587
Josseph <jossephalvarez@gmail.com>
2021-04-28 09:02:30 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider_architecture_boilerplate/ui/views/home/home_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Boilerplate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(),
+    );
+  }
+}
